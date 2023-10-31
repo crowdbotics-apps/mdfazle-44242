e21a85d03cb0b92c176abf9d6a44565d568e4c0c
Crowdbotics <team@crowdbotics.com>
2023-10-31 08:21:58 +0000

    Initial commit

---
+++ backend/mdfazle_44242/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for mdfazle_44242 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "mdfazle_44242.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mdfazle-44242",
+  url: "https://mdfazle-44242.botics.co",
+  api: "https://mdfazle-44242.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
